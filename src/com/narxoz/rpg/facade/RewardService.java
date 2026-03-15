41600d66fe5ba8af6dc504be204947aa10d179d0
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-03-15 12:26:48 +0500

    dungeonfacade that works by the principle preparation, battle, reward is ready and reward service with who is winner is also ready

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,9 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
+        if (battleResult == null||battleResult.getWinner()==null) {
+            return "battle reward is unknown";
         }
-        return "TODO";
-    }
-}
+        if(battleResult.getWinner().equalsIgnoreCase("Hero")){return "67 gold coins and epic chest";}
+        else{return "you were defeated";}
+}}
